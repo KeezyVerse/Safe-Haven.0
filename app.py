fe1f915c7f5a95935a018383ae595a424ee246f4
KeezyVerse <144798391+KeezyVerse@users.noreply.github.com>
2023-11-21 21:34:00 +0000

    Updated flask

--- app.py
+++ app.py
@@ -2,31 +2,40 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comments.db'
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a random secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///safehaven.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('comments', lazy=True))
 
 @app.route('/')
 def home():
-    comments = Comment.query.all()
-    return render_template('index.html', comments=comments)
+    return render_template('home.html', comments=Comment.query.all())
 
-@app.route('/submit_post', methods=['POST'])
-def submit_post():
-    username = request.form['username']
-    email = request.form['email']
-    content = request.form['content']
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    new_comment = Comment(username=username, email=email, content=content)
-    db.session.add(new_comment)
-    db.session.commit()
+        # Check if the user exists (simple authentication)
+        user = User.query.filter_by(username=username, password=password).first()
 
-    return redirect(url_for('home'))
+        if user:
+            return redirect(url_for('home'))
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
     db.create_all()
