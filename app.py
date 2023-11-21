1520ec3f0cdfe077904595e245f036ac6066eafc
KeezyVerse <144798391+KeezyVerse@users.noreply.github.com>
2023-11-21 22:08:28 +0000

    code

--- app.py
+++ app.py
@@ -1,42 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a random secret key
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///safehaven.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+app.secret_key = 'secret key'
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-
-class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('comments', lazy=True))
-
-@app.route('/')
-def home():
-    return render_template('home.html', comments=Comment.query.all())
+users = {'Jimmy': 'Drool'}
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        user = request.form['username']
         password = request.form['password']
-
-        # Check if the user exists (simple authentication)
-        user = User.query.filter_by(username=username, password=password).first()
-
-        if user:
+        if user in users and users[user] == password:
             return redirect(url_for('home'))
-
+        return 'Login unsuccessful'
     return render_template('login.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
